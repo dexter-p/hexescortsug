64560dc032b3d6a01ee74de6e0d8d11ea0660995
Bankai <203122552+dexter-p@users.noreply.github.com>
2026-03-21 22:40:07 +0300

    Force dynamic homepage to restore profile shuffling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import HomePage from "@/screens/HomePage";
 import { fetchAllProfiles } from "@/data/allProfiles";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const initialProfiles = await fetchAllProfiles();
   return <HomePage initialProfiles={initialProfiles} />;
